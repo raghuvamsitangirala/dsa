b08764cb1caf78c800ec17671c861c037d2016e3
Raghuvamsi Tangirala <raghuvamsitangirala@gmail.com>
2020-10-06 16:32:39 -0400

    Added Problem 160

---
+++ src/practice/LeetCode/Problem160.java
@@ -0,0 +1,80 @@
+public class Problem160 {
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(4);
+        node1.next = new ListNode(1);
+        node1.next.next = new ListNode(8);
+        node1.next.next.next = new ListNode(4);
+        node1.next.next.next.next = new ListNode(5);
+
+        ListNode node2 = new ListNode(5);
+        node2.next = new ListNode(6);
+        node2.next.next = new ListNode(1);
+        node2.next.next.next = new ListNode(8);
+        node2.next.next.next.next = new ListNode(4);
+        node2.next.next.next.next.next = new ListNode(5);
+
+        Problem21 solution1 = new Problem21();
+        ListNode newNode = solution1.mergeTwoLists(node1,node2);
+        ListNode temp = newNode;
+        while(temp != null){
+            System.out.print(temp.val + "->");
+            temp = temp.next;
+        }
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int len1 = 0;
+        int len2 = 0;
+        ListNode p1=headA, p2=headB;
+        if (p1 == null || p2 == null)
+            return null;
+ 
+        while(p1 != null){
+            len1++;
+            p1 = p1.next;
+        }
+        while(p2 !=null){
+            len2++;
+            p2 = p2.next;
+        }
+ 
+        int diff = 0;
+        p1=headA;
+        p2=headB;
+ 
+        if(len1 > len2){
+            diff = len1-len2;
+            int i=0;
+            while(i<diff){
+                p1 = p1.next;
+                i++;
+            }
+        }else{
+            diff = len2-len1;
+            int i=0;
+            while(i<diff){
+                p2 = p2.next;
+                i++;
+            }
+        }
+ 
+        while(p1 != null && p2 != null){
+            if(p1.val == p2.val){
+                return p1;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+ 
+        return null;
+    }
+}
+
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
