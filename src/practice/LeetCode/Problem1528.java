6242bf08b2d2bff56cd4e85ea306c6e0ee21c6f5
Raghuvamsi Tangirala <raghuvamsitangirala@gmail.com>
2020-10-15 16:16:38 -0400

    Added Problem1528.java

---
+++ src/practice/LeetCode/Problem1528.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Problem1528 {
+    public String restoreString(String s, int[] indices) {
+        char[] restored = new char[indices.length];
+        
+        for (int i = 0; i < s.length(); i++) {
+           restored[indices[i]] = s.charAt(i);
+        }
+        
+        return new String(restored);
+    }
+
+    public static void main(String[] args) {
+        Problem1528 p = new Problem1528();
+        String s = "codeleet";
+        int[] indices = {4,5,6,7,0,2,1,3};
+        String result = p.restoreString(s, indices);
+        System.out.println(result);
+    }
+}
